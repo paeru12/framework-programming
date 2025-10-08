d2fbd94bfb0f2285b3eb655cfdea235ac51daadf
paeru12 <herupratama8525@gmail.com>
2025-10-08 12:23:59 +0700

    pertemuan-4

--- project-2/kelurahan_app/warga/urls.py
+++ project-2/kelurahan_app/warga/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanCreateView
+from .views import (
+    WargaListView,
+    WargaDetailView,
+    PengaduanCreateView,
+    PengaduanUpdateView,
+    PengaduanDeleteView,
+)
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+
+    # URL pengaduan
     path('pengaduan/tambah/<int:warga_id>/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+    path('pengaduan/<int:pk>/edit/', PengaduanUpdateView.as_view(), name='pengaduan-edit'),
+    path('pengaduan/<int:pk>/hapus/', PengaduanDeleteView.as_view(), name='pengaduan-hapus'),
 ]
--- project-2/kelurahan_app/warga/views.py
+++ project-2/kelurahan_app/warga/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import PengaduanForm
 
 class WargaListView(ListView):
     model = Warga
+    template_name = 'warga/warga_list.html'
+
 
 class WargaDetailView(DetailView):
     model = Warga
+    template_name = 'warga/warga_detail.html'
+
 
 class PengaduanCreateView(CreateView):
     model = Pengaduan
@@ -22,9 +25,39 @@ class PengaduanCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
+        # setelah pengaduan dibuat, kembali ke detail warga
         return reverse_lazy('warga-detail', kwargs={'pk': self.kwargs['warga_id']})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['warga_id'] = self.kwargs['warga_id']
         return context
+
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['warga_id'] = self.object.pelapor.id
+        return context
+    
+    def form_valid(self, form):
+        if not form.instance.pelapor:
+            form.instance.pelapor = self.object.pelapor
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        pelapor = self.object.pelapor
+        if pelapor: 
+            return reverse_lazy('warga-detail', kwargs={'pk': pelapor.pk})
+        else:
+            return reverse_lazy('warga-list')
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('warga-detail', kwargs={'pk': self.object.pelapor.pk})
